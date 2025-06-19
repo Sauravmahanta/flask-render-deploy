4a40f4898406991e9c50c60c18efd32be37c87ce
SAURAV KUMAR MAHANTA <mahanta.saurav5@gmail.com>
2025-06-19 19:56:20 +0530

    first commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 #app routing
 @app.route('/', methods=['get'])
 def home():
-    return 'Welcome to home page!'
+    return 'Hello Saurav! Welcome to home page!'
 
 if __name__=='__main__':
     app.run(debug='true')
